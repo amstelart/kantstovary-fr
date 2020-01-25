89a921e77f33f97dfa69e3131374643f814732c2
amstelart <amsteldroid@gmail.com>
2020-01-25 13:19:39 +0300

    dev-2

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -80,23 +80,10 @@ $( document ).ready(function() {
 	});
 	function setrating(th,val) {
 		th.find('.rating__activeline').css({width:val+'%'});
-	}
+  }
+  
+  $('.collapsable-slide').collapsable({
+    fx: 'slide',
+    fxDuration: 300
+  });
 });
-
-// Изоляция без jQuery
-// (function(){
-//   // code
-// }());
-
-// На проекте нет jQuery, но хочется $( document ).ready...
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
-//
-// ready(function(){
-//   // code
-// });
